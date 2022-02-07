cb9e85d965455762ebf2ffeba06a1fc5d68c11d9
Shubham <s.shrikhande1@gmail.com>
2022-02-07 21:59:49 +0530

    [shubham]:Added oop concept file

---
+++ Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcept
+{
+    public class Inventory
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace OOPSConcept
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            InventoryManagement inventoryManagement = new InventoryManagement();
+            inventoryManagement.ReadData(@"D:\OOPS1\Inventory.json");
+            Console.ReadKey();
+        }
+        
+    }
+}
