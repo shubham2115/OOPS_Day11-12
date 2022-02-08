8abeb3e299396e4f0eb06c10c7fefc86e642d171
Shubham <s.shrikhande1@gmail.com>
2022-02-09 02:31:46 +0530

    [shubham]:Added this file of stock report

---
+++ Stock_Exchange/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Exchange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StockData data = new StockData();
+            data.StockList(@"D:\Stock_Exchange\Stock_Exchange\Stock.json");
+        }
+    }
+}
